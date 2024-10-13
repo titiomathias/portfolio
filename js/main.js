615658385765a701abd2a07a7cbdd54d716e4364
Matheus de Alencar <matheusdealencar.contato@gmail.com>
2024-10-13 13:19:06 -0300

    styling other divs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const header = document.querySelector(".navbar")
 
 window.addEventListener("scroll", () => {
-    if(window.scrollY >= 450){
+    if(window.scrollY >= 150){
         header.classList.add("scrolled")
     } else {
         header.classList.remove("scrolled")
